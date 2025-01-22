a24f8fe1e41cf260ba493d149c3fb4b429f7ac97
halamkova <Lidmila-Marie.Halamkova@gjkt.cz>
2025-01-22 14:49:02 +0100

    Update CSV_Format.py

--- CSV_Format.py
+++ CSV_Format.py
@@ -1,10 +1,9 @@
 import csv
+
 class CSV_Format:
-    #
-    def __init__(self, nacitane_bludiste):
-        self.nacitane_bludiste = nacitane_bludiste
-    def nacist_soubor(self):
-        with open(self.nacitane_bludiste, 'r') as file:
+    def kod(self, nacitane_bludiste):
+    #stejny pro csv
+        with open(nacitane_bludiste, 'r') as file:
             reader = csv.reader(file)
             self.content = list(reader)
-        return self.content
+            return self.content
